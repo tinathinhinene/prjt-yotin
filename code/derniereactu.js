706386e1757669255290fc70f8caed8b37ae7030
YoaN80490 <147514201+YoaN80490@users.noreply.github.com>
2024-01-02 15:56:54 +0100

    02/01/2024 15H56

--- code/derniereactu.js
+++ code/derniereactu.js
@@ -4,18 +4,5 @@ croix.addEventListener("click", () => {
 })
 
 
-const change = (int) => {
-    if ($('#infosup'+int).is(':visible')) {
-        $('#infosup'+int).hide();
-        document.getElementById('arrow'+int).style.WebkitTransitionDuration='0.75s';
-        document.getElementById('arrow'+int).style.rotate ='0deg';
-    }
-    else {
-        $('#infosup'+int).show();
-        document.getElementById('arrow'+int).style.WebkitTransitionDuration='0.75s';
-        document.getElementById('arrow'+int).style.rotate ='180deg';
-        
-    }
-}
 
 
